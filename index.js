b09d2675750d73ee6ca9faf48984facb3f271652
Jacques Granarolo <jac.granarolo.info@gmail.com>
2023-07-09 20:00:15 +0200

    mise en ligne serveur json-server

---
+++ index.js
@@ -0,0 +1,9 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 4250;
+
+server.use(middlewares);
+server.use(router);
+server.listen(port);
