73a58afa1e7bf80d1ca7f4d95131c2f44a3d855b
Jacques Granarolo <jac.granarolo.info@gmail.com>
2023-07-22 18:10:43 +0200

    modifs package.json

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ const port = process.env.PORT || 4250;
 
 server.use(middlewares);
 server.use(router);
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
