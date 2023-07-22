48a3f6105df30aab600813c64d49da815770cc4e
Jacques Granarolo <jac.granarolo.info@gmail.com>
2023-07-22 18:30:52 +0200

    modifs package.json

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 4250;
 
 server.use(middlewares);
 server.use(router);
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
+
+module.exports = server;
